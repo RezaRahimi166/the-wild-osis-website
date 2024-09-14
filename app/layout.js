33c14ac78c20d0047fdd5c5dfe0dea722612bc18
RezaRahimi166 <rezaa.salami99@gmail.com>
2024-09-14 11:37:19 -0800

    instaling reacicons && dding fonts

--- app/layout.js
+++ app/layout.js
@@ -1,14 +1,28 @@
 import Logo from "./_components/Logo";
 import Navigation from "./_components/Navigation";
+import "@/app/_styles/globals.css";
+
+import { Josefin_Sans } from "next/font/google";
+const josefin = Josefin_Sans({
+  subsets: ["latin"],
+  display: "swap",
+});
 
 export const metadata = {
-  title: "The Wild Oasis",
+  title: {
+    template: "%s | The Wild Oasis",
+    default: "Welcome / The Wild Oasis",
+  },
+  description:
+    "Luxurios cabin hotel , located in the heart of italyain dolomits, surrounded by beatiful mountains and oceans",
 };
 
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
-      <body>
+      <body
+        className={`${josefin.className}min-h-screen text-primary-100 bg-primary-950`}
+      >
         <header>
           <Logo />
           <Navigation />
