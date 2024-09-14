05cb3973a3fe246a5562da41f9b7d4d22a27627b
RezaRahimi166 <rezaa.salami99@gmail.com>
2024-09-14 07:46:01 -0800

    creating navigation and layout and adding starter files

--- app/page.js
+++ app/page.js
@@ -1,7 +1,17 @@
+import Link from "next/link";
+import Navigation from "./components/Navigation";
+
 export default function Page() {
   return (
-    <h1 className="flex items-center justify-center">
-      The wild osis welcome to the paradise!
-    </h1>
+    <>
+      <div>
+        <Navigation />
+
+        <h1 className="flex items-center justify-center">
+          The wild osis welcome to the Paradise!
+        </h1>
+        <Link href="/cabins">Explore luxury cabins</Link>
+      </div>
+    </>
   );
 }
