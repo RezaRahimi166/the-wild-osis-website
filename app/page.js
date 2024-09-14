a1a7810b9fc346fb89f2170e00ce48a489d81cde
RezaRahimi166 <rezaa.salami99@gmail.com>
2024-09-13 17:54:26 -0800

    setting up pges

--- app/page.js
+++ app/page.js
@@ -1,3 +1,7 @@
 export default function Page() {
-  return <h1 className="flex items-center justify-center">Hello Next!</h1>;
+  return (
+    <h1 className="flex items-center justify-center">
+      The wild osis welcome to the paradise!
+    </h1>
+  );
 }
