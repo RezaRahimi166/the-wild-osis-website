f880f58852bb67ed5819cc754409ba8ae7eb409f
RezaRahimi166 <rezaa.salami99@gmail.com>
2024-09-15 08:43:31 -0800

    data cabins fetching and spasific layoout for cabins

--- starter/pages/cabins-page.js
+++ starter/pages/cabins-page.js
@@ -1,8 +1,10 @@
 import CabinCard from "@/app/_components/CabinCard";
+import { getCabins } from "@/app/_lib/data-service";
 
-export default function Page() {
-  // CHANGE
-  const cabins = [];
+export default async function Page() {
+  console.log("Starting...");
+  const cabins = await getCabins();
+  console.log(cabins);
 
   return (
     <div>
@@ -13,9 +15,9 @@ export default function Page() {
         Cozy yet luxurious cabins, located right in the heart of the Italian
         Dolomites. Imagine waking up to beautiful mountain views, spending your
         days exploring the dark forests around, or just relaxing in your private
-        hot tub under the stars. Enjoy nature's beauty in your own little home
-        away from home. The perfect spot for a peaceful, calm vacation. Welcome
-        to paradise.
+        hot tub under the stars. Enjoy nature&apos;s beauty in your own little
+        home away from home. The perfect spot for a peaceful, calm vacation.
+        Welcome to paradise.
       </p>
 
       {cabins.length > 0 && (
