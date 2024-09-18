9f941a52ee0b2e404c106d32a791387c65263157
RezaRahimi166 <rezaa.salami99@gmail.com>
2024-09-18 11:16:41 -0800

    applying filter for cabins page

--- app/cabins/page.js
+++ app/cabins/page.js
@@ -1,4 +1,8 @@
+import { Suspense } from "react";
 import CabinCard from "../_components/CabinCard";
+import CabinList from "../_components/CabinList";
+import Spinner from "../_components/Spinner";
+import Filter from "../_components/Filter";
 
 // to updating data from catche
 export const revalidate = 0;
@@ -7,7 +11,9 @@ export const metadata = {
   title: "Cabins",
 };
 
-export default function Page() {
+export default function Page({ searchParams }) {
+  const filter = searchParams?.capacity ?? "all";
+
   // CHANGE
   const cabins = [];
 
@@ -25,13 +31,12 @@ export default function Page() {
         Welcome to paradise.
       </p>
 
-      {cabins.length > 0 && (
-        <div className="grid sm:grid-cols-1 md:grid-cols-2 gap-8 lg:gap-12 xl:gap-14">
-          {cabins.map((cabin) => (
-            <CabinCard cabin={cabin} key={cabin.id} />
-          ))}
-        </div>
-      )}
+      <div className="flex justify-end mb-8">
+        <Filter />
+      </div>
+      <Suspense fallback={<Spinner />} key={filter}>
+        <CabinList filter={filter} />
+      </Suspense>
     </div>
   );
 }
