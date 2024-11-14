33a59768aa79da615655ba1c9eeab55e4c355b9e
RezaRahimi166 <rezaa.salami99@gmail.com>
2024-11-14 08:48:14 -0900

    feat(auth): applyed to components

--- app/_components/Reservation.js
+++ app/_components/Reservation.js
@@ -1,5 +1,7 @@
+import { auth } from "../_lib/auth";
 import { getBookedDatesByCabinId, getSettings } from "../_lib/data-service";
 import DateSelector from "./DateSelector";
+import LoginMessage from "./LoginMessage";
 import ReservationForm from "./ReservationForm";
 
 async function Reservation({ cabin }) {
@@ -7,14 +9,21 @@ async function Reservation({ cabin }) {
     getSettings(),
     getBookedDatesByCabinId(cabin.id),
   ]);
+
+  const session = await auth();
+
   return (
-    <div className="flex gap-8 flex-col-reverse border border-primary-800 min-h-[400px]">
+    <div className="flex gap-8flex-col-reverse border border-primary-800 min-h-[400px]">
       <DateSelector
         settings={settings}
         bookedDates={bookedDates}
         cabin={cabin}
       />
-      <ReservationForm cabin={cabin} />
+      {session?.user ? (
+        <ReservationForm cabin={cabin} user={session.user} />
+      ) : (
+        <LoginMessage />
+      )}
     </div>
   );
 }
