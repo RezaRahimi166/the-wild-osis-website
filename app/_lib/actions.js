60ae6016c463857e8e73b4f10bea45fef45be93d
RezaRahimi166 <rezaa.salami99@gmail.com>
2024-11-16 15:11:34 -0900

    feat(forms): using serverActions and revalidatePath

--- app/_lib/actions.js
+++ app/_lib/actions.js
@@ -1,6 +1,31 @@
 "use server";
 
-import { signIn, signOut } from "./auth";
+import { revalidatePath } from "next/cache";
+import { auth, signIn, signOut } from "./auth";
+import { supabase } from "./supabase";
+
+export async function updateProfile(formData) {
+  const session = await auth();
+  if (!session) throw new Error("You must be logged in");
+
+  const nationalID = formData.get("nationalID");
+  const [nationality, countryFlag] = formData.get("nationality").split("%");
+
+  if (!/^[a-zA-Z0-9]{6,12}$/.test(nationalID))
+    throw new Error("please provide a valid nationalId");
+
+  const updatedData = { nationality, countryFlag, nationalID };
+
+  // sendData to backend
+  const { data, error } = await supabase
+    .from("guests")
+    .update(updatedData)
+    .eq("id", session.user.guestId);
+
+  if (error) throw new Error("Guest could not be updated");
+
+  revalidatePath("/account/profile");
+}
 
 export async function signInAction() {
   await signIn("google", { redirectTo: "/account" });
